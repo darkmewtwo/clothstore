7221ed9c07f5b4c77255e876491691948d51bf97
darkmewtwo <ashleysunil07@gmail.com>
2023-07-19 00:27:30 +0530

    dockerized and added psql as database

--- clothstore/settings.py
+++ clothstore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'clothstore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'clothstore',
+        'USER': 'clothstore_user',
+        'PASSWORD': 'clothpass',
+        'HOST': 'pdb',
+        'PORT': '5432'
     }
 }
 
